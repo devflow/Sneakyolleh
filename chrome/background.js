cc0430b59db50d46458edb9d4582ddbf7855f6f6
Ahn kae-seong <regame@youdie.net>
2016-04-25 15:39:18 +0900

    remove some useless

--- chrome/background.js
+++ chrome/background.js
@@ -1,10 +1,3 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Simple extension to replace lolcat images from
-// http://icanhascheezburger.com/ with loldog images instead.
-
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function(info) {
     var ret = {};
